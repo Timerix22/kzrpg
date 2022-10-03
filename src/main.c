2016b9c29fc21d579c84b5e664828922fb014280
timerix <timerix11@gmail.com>
2022-10-03 11:42:51 +0600

    added sdl

--- src/main.c
+++ src/main.c
@@ -1,13 +1,21 @@
 #include <GL/gl.h>
+#define SDL_MAIN_HANDLED
+#include <SDL/SDL.h>
 #include "../kerep/src/base/base.h"
+
+#define sdlErrCheck(_STATEMENT) if(_STATEMENT) throw(cptr_concat("SDL error:\n" #_STATEMENT "\n", SDL_GetError()))
 
 int main(){
     setlocale(LC_ALL, "en-US.Unicode");
+    printf("hello!\n");
     ktDescriptors_beginInit();
     ktDescriptors_initKerepTypes();
     ktDescriptors_endInit();
-    printf("hello!\n");
     ktDescriptor desc=ktDescriptor_get(ktId_Bool);
     printf("name: %s id: %u size: %u\n", desc.name, desc.id, desc.size);
+    sdlErrCheck(SDL_Init(SDL_INIT_VIDEO)!=0);
+    SDL_Window* win=SDL_CreateWindow("UwU",50,50,666,228, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+    sdlErrCheck(win==NULL);
+    getc(stdin);
     return 0;
 }
--- tasks/pre_build.sh
+++ tasks/pre_build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
+# compiling kerep
 cd kerep
 make $KEREP_BUILD_TASK
 cp bin/kerep.a ../obj/
 printf "${GREEN}copied ${CYAN}kerep.a\n"
 cd ..
+
+# copying SDL
+cp deps/$OS-$ARCH/$SDL_LIB_NAME $OUTDIR/$SDL_LIB_NAME
